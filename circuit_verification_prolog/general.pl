5cf797727e634c921c2fc29439a8fece5c568c6c
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-03-21 15:56:53 +0530

    full adder

--- circuit_verification_prolog/general.pl
+++ circuit_verification_prolog/general.pl
@@ -1,41 +1,39 @@
-not(A):- A, !, fail.
-not(A).
-verify(not,[L],B):- B, !, not(L).
-verify(not,[L],B):- not(not(L)).
+notop(true):-false,!.
+notop(false):-true,!.
 
-and(A,B):- A, !, B.
-and([]).
-and([A|B]):- and(A, and(B)).
-verify(and,L,B):- B, !, and(L).
-verify(and,L,B):- not(and(L)).
+notop(A):- A, !, fail.
+notop(A).
 
-or(A,B):- A, !, true.
-or(A,B):- B, !, true.
-or([]):- false.
-or([A|B]):- or(A, or(B)).
-verify(or,L,B):- B, !, or(L).
-verify(or,L,B):- not(or(L)).
+andop(A,B):- A, !, B.
 
-xor(A,B):-or(and(A,not(B)),and(not(A),B)).
-xor([]):-false.
-xor([A|B]):-xor(A, xor(B)).
+andop(true,true):-true,!.
+andop(true,false):-false,!.
+andop(false,true):-false,!.
+andop(false,false):-false,!.
 
-verify(xor,L,B):- B, !, xor(L).
-verify(xor,L,B):- not(xor(L)).
+orop(A,B):- A, !, true.
+orop(A,B):- B, !, true.
+
+orop(false,false):-false,!.
+orop(true,false):-true,!.
+orop(false,true):-true,!.
+orop(true,true):-true,!.
+
+xorop(A,B):-orop(andop(A,notop(B)),andop(notop(A),B)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-in(N, X):- and(connected(A, in(N, X)), terminal(A)), !, signal(A).
+in(N, X):- andop(connected(A, in(N, X)), terminal(A)), !, signal(A).
 in(N, X):- connected(A, in(N, X)), !, out(A).
  
-out(X):- type(X, and), connected(A,in(1,X)), connected(B,in(2,X)), !, and(in(1,X), in(2, X)).
-out(X):- type(X, and), write('circuit is open'),!.
-out(X):- type(X, or), connected(A,in(1,X)), connected(B,in(2,X)), !, or(in(1,X), in(2, X)).
-out(X):- type(X, or), write('circuit is open'),!.
-out(X):- type(X, not), connected(A,in(1,X)), !, not(in(1,X)).
-out(X):- type(X, not), write('circuit is open'),!.
-out(X):- type(X, xor), connected(A,in(1,X)), connected(B,in(2,X)), !, xor(in(1,X), in(2, X)).
-out(X):- type(X, xor), write('circuit is open'),!.
+out(X):- type(X, andop), connected(A,in(1,X)), connected(B,in(2,X)), !, andop(in(1,X), in(2, X)).
+out(X):- type(X, andop), write('circuit is open'),!.
+out(X):- type(X, orop), connected(A,in(1,X)), connected(B,in(2,X)), !, orop(in(1,X), in(2, X)).
+out(X):- type(X, orop), write('circuit is open'),!.
+out(X):- type(X, notop), connected(A,in(1,X)), !, notop(in(1,X)).
+out(X):- type(X, notop), write('circuit is open'),!.
+out(X):- type(X, xorop), connected(A,in(1,X)), connected(B,in(2,X)), !, xorop(in(1,X), in(2, X)).
+out(X):- type(X, xorop), write('circuit is open'),!.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -45,7 +43,7 @@ goal(A,X):-retractall(signal(_)),fn(A,1),!,check(X).
 fn([], L).
 fn([A|B],L):-A,!,assert(signal(L)), increment(L,L1), fn(B, L1).
 fn([A|B],L):-increment(L,L1), fn(B, L1).
-check(X):-not(xor(output,X)).
+check(X):-notop(xorop(output,X)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
