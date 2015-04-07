8debe667f85bb4af1d193f887d241acfcf95f530
Bhargav Chippada <bhargav@bhargav-Inspiron-5523>
2015-04-07 14:22:26 +0530

    goal variable computing complete

--- circuit_verification_prolog/xor.pl
+++ circuit_verification_prolog/xor.pl
@@ -1,4 +1,4 @@
-:- [general].
+:- [test].
 
 terminal(1).
 terminal(2).
@@ -11,10 +11,10 @@ connected(g2,in(2,g4)).
 connected(g3,in(1,g5)).
 connected(g4,in(2,g5)).
 
-type(g1,not).
-type(g2,not).
-type(g3,and).
-type(g4,and).
-type(g5,or).
+type(g1,notop).
+type(g2,notop).
+type(g3,andop).
+type(g4,andop).
+type(g5,orop).
 
 output :- out(g5).
